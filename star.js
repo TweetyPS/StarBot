c46c094497041273bdc7c9a0ab0e9cfaba083e80
Magno-D <40536442+Magno-D@users.noreply.github.com>
2018-08-06 22:34:50 +0300

    Update star.js

--- star.js
+++ star.js
@@ -186,7 +186,6 @@ if (message.content === prefix + 'help') {
 『**${prefix}**move all / لسحب جميع الأعضاء لرومك الصوتي』
 『**${prefix}**hchannel / لاخفاء الشات』
 『**${prefix}**schannel / لاظهار الشات』
-『**${prefix}**vonline /(لرؤية كم عضو في رومات الصوت (طبعا البوت بسوي روم لحالو ما يحتاج تسي روم』
 『log / لتفعيل اللوق يجب عمل شات بأسم』
 『report / لتفعيل البلاغ يجب عمل شات بأسم』
 『warns / لتفعيل تحذير العضو يجب عمل شات بأسم』
@@ -4191,21 +4190,6 @@ client.users.forEach(m =>{
 m.sendMessage(args)
 })
 }
-});
-client.on('message',async Epic => {
-  if(Epic.content.startsWith(prefix + "vonline")) {
-  if(!Epic.guild.member(Epic.author).hasPermissions('MANAGE_CHANNELS')) return Epic.reply(':x: **ليس لديك الصلاحيات الكافية**');
-  if(!Epic.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return Epic.reply(':x: **ليس معي الصلاحيات الكافية**');
-  Epic.guild.createChannel(`Voice Online : [ ${Epic.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
-    console.log(`Voice Online Is Activation In ${Epic.guild.name}`);
-    c.overwritePermissions(Epic.guild.id, {
-      CONNECT: false,
-      SPEAK: false
-    });
-    setInterval(() => {
-      c.setName(` Online :  ${Epic.guild.members.filter(m => m.voiceChannel).size} .`)
-    },1000);
-  }
 });
 client.on("ready", function() {
 	console.log("ready");
